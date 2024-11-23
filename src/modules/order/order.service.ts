23c8cf7d0344107447717189e2422bb8fca33078
Th3At0nic <islammdrahatul@gmail.com>
2024-11-24 01:53:23 +0600

    successfully done creating order+inventory management using session.transaction

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,16 +1,65 @@
+import mongoose from 'mongoose';
 import { TOrder } from './order.interface';
 import { OrderModel } from './order.model';
+import { CarModel } from '../car/car.model';
 
 //creating an order to DB
-const createOrderIntoDB = async (order: TOrder) => {
+const createOrderWithInventoryManagementIntoDB = async (order: TOrder) => {
+  const session = await mongoose.startSession(); // creating a session from the mongoose
   try {
-    const result = await OrderModel.create(order);
+    //starting a transaction with the session which allow us to execute multiple opt together, or cancel together.
+    session.startTransaction();
+
+    // Step 1: Find the car being ordered to check its availability and stock
+    const car = await CarModel.findById(order.car).session(session);
+
+    if (!car) {
+      throw new Error('Car not found!');
+    }
+    if (car.quantity < order.quantity && car.quantity >= 1) {
+      throw new Error(
+        `Insufficient stock! Only ${car.quantity} unit available.`,
+      );
+    }
+
+    // reducing the order quantity from the car quantity and assigning the new quantity to the car quantity
+    car.quantity -= order.quantity;
+
+    //updating the car inStock status
+    if (car.quantity === 0) {
+      car.inStock = false;
+    }
+
+    // saving the updated car details into the DB with the session
+    await car.save({ session });
+
+    //creating a new order in the DB
+    const result = await OrderModel.create([order], { session });
+
+    // committing the transaction to finalize all the changes into the DB
+    await session.commitTransaction();
+
+    // finishing the session as the all job is done by session successfully
+    session.endSession();
+
+    //finally returning the result of created order. also inventory management done.
     return result;
   } catch (err) {
+    // if any error occur, abort all the transaction to undo all the changes including the inventory and order
+    await session.abortTransaction();
+
+    session.endSession();
+
     throw new Error('An error occur ordering a car!' + err);
   }
 };
 
+//
+//
+//
+//
+//
+//
 // calculating the total revenue from orders
 const calcRevenueFromOrders = async () => {
   try {
@@ -53,6 +102,6 @@ const calcRevenueFromOrders = async () => {
 };
 
 export const OrderService = {
-  createOrderIntoDB,
+  createOrderIntoDB: createOrderWithInventoryManagementIntoDB,
   calcRevenueFromOrders,
 };
