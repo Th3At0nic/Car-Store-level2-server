54296df5a4bf56862402a75da12cf14173dba0f1
Th3At0nic <islammdrahatul@gmail.com>
2024-11-24 16:29:03 +0600

    modified a little, final push for order module

--- src/modules/order/order.validation.ts
+++ src/modules/order/order.validation.ts
@@ -1,7 +1,24 @@
 import { z } from 'zod';
 import { Types } from 'mongoose';
 
-// Defining a type for the error structure
+// Schema for validating a MongoDB ObjectId
+const objectIdSchema = z.string().refine((id) => Types.ObjectId.isValid(id), {
+  message: 'Invalid ObjectId format',
+});
+
+// Schema for validating the structure of an order
+export const orderValidationSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  car: objectIdSchema,
+  quantity: z
+    .number()
+    .positive({ message: 'Quantity must be a positive number' }),
+  totalPrice: z
+    .number()
+    .positive({ message: 'Total price must be a positive number' }),
+});
+
+// Defining a type for the error structure in the order controller file
 export type ErrorType = {
   path: string[];
   message: string;
@@ -24,20 +41,3 @@ export type AccType = {
     value: number;
   };
 };
-
-// Schema for validating a MongoDB ObjectId
-const objectIdSchema = z.string().refine((id) => Types.ObjectId.isValid(id), {
-  message: 'Invalid ObjectId format',
-});
-
-// Schema for validating the structure of an order
-export const orderValidationSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
-  car: objectIdSchema,
-  quantity: z
-    .number()
-    .positive({ message: 'Quantity must be a positive number' }),
-  totalPrice: z
-    .number()
-    .positive({ message: 'Total price must be a positive number' }),
-});
