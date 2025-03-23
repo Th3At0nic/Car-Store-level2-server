24eeccc56de909f955c3790f7fd7309fa3628c2d
Th3At0nic <islammdrahatul@gmail.com>
2025-03-23 15:45:41 +0600

    remove auth guard to get cars, made public route

--- src/app/modules/car/car.route.ts
+++ src/app/modules/car/car.route.ts
@@ -1,19 +1,17 @@
 import express from 'express';
 import { CarControllers } from './car.controller';
-import { auth } from '../../middlewares/authRequest';
-import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.get(
   '/',
-  auth(USER_ROLE.admin, USER_ROLE.user),
+  // auth(USER_ROLE.admin, USER_ROLE.user),
   CarControllers.getAllCars,
 );
 
 router.get(
   '/:carId',
-  auth(USER_ROLE.admin, USER_ROLE.user),
+  // auth(USER_ROLE.admin, USER_ROLE.user),
   CarControllers.getACarById,
 );
 
