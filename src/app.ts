df555ba6e924a52bc0ded2648b45273b1c00904b
Th3At0nic <islammdrahatul@gmai.com>
2025-04-15 16:41:12 +0600

    populated car into getMyOrders

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(
-  cors({ origin: 'http://localhost:5173', credentials: true }), //https://carstore-client.vercel.app
+  cors({ origin: 'https://carstore-client.vercel.app', credentials: true }), //http://localhost:5173
 );
 
 app.use('/api', router);
