99c6ecbdc1a0ec59812ae53fdf5f7f64166024f9
Th3At0nic <islammdrahatul@gmai.com>
2025-04-15 12:50:34 +0600

    modified sentImageToCLoudinary and app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(
-  cors({ origin: 'http://localhost:5173', credentials: true }), //https://carstore-client.vercel.app
+  cors({ origin: 'https://carstore-client.vercel.app', credentials: true }), //https://carstore-client.vercel.app
 );
 
 app.use('/api', router);
@@ -23,3 +23,5 @@ app.use(globalErrorHandler);
 app.use(notFound);
 
 export default app;
+
+//http://localhost:5173
